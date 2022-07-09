dd5ed14742c282d3cb2ca390a536861145f06449
david <david.mosca69@gmail.com>
2022-07-09 17:18:25 +0200

    add css for header

---
+++ front/src/features/nav/index.tsx
@@ -0,0 +1,7 @@
+import styles from './nav.module.scss';
+
+function Nav() {
+  return <nav className={styles.nav}>Nav</nav>;
+}
+
+export default Nav;
--- front/src/layout/header/index.tsx
+++ front/src/layout/header/index.tsx
@@ -1,5 +1,14 @@
+import Auth from '../../features/auth';
+import Nav from '../../features/nav';
+import styles from './header.module.scss';
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <div>logo</div>
+      <Nav />
+      <Auth />
+    </header>
+  );
 }
 
 export default Header;
