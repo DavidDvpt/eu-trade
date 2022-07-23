f7e5d08e70f5fd4d3b4e7de3f6889e6350982dee
david <david.mosca69@gmail.com>
2022-07-23 14:30:49 +0200

    add and configure eslint and rettier

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json('Hello Word !!!');
+router.get("/", (req, res) => {
+  res.status(200).json("Hello Word !!!");
 });
 
 export default router;
