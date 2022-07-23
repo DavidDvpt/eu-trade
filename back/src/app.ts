6ec36376200be3267a407084117316b7e98fee5a
david <david.mosca69@gmail.com>
2022-07-23 11:46:37 +0200

    add prettier, husky and eslint plugins

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,14 +1,14 @@
-import express, { Express } from "express";
-import helmet from "helmet";
-import morgan from "morgan";
+import express, { Express } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
-import routes from "./routes";
+import routes from './routes';
 
 const app: Express = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(helmet());
 
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
 
 export default app;
