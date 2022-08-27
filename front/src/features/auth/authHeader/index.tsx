0e256eedacd90b5ec0a5a1c2b48f842263ddbfbc
david <david.mosca69@gmail.com>
2022-08-27 16:23:46 +0200

    update header account css

--- front/src/features/auth/authHeader/index.tsx
+++ front/src/features/auth/authHeader/index.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { getAuthState, logout, setLoginModal } from '../authSlice';
 import styles from './authHeader.module.scss';
@@ -13,25 +11,47 @@ function AuthHeader() {
         dispatch(setLoginModal(true));
     };
 
+    const handleAccountClick = () => {};
+
     const handleLogoutClick = () => {
         dispatch(logout());
     };
 
     return (
         <div className={styles.authHeader}>
-            {!isLogged ? (
-                <div className={styles.loginBloc}>
-                    <span onClick={handleLoginClick}>Login</span>
-                    <a href="#">Créer un compte</a>
-                </div>
-            ) : (
-                <div className={styles.accountBloc}>
-                    <span>{userPseudo}</span>
-                    <Link to="/" onClick={handleLogoutClick}>
-                        Se déconnecter
-                    </Link>
-                </div>
-            )}
+            <div className={styles.accountBloc}>
+                {!isLogged ? (
+                    <>
+                        <div
+                            className={styles.userDisplay}
+                            onClick={handleLoginClick}
+                        >
+                            Login
+                        </div>
+                        <div className={styles.buttons}>
+                            <div className={styles.button}>Créer un compte</div>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className={styles.userDisplay}>{userPseudo}</div>
+                        <div className={styles.buttons}>
+                            <div
+                                className={styles.button}
+                                onClick={handleAccountClick}
+                            >
+                                Account
+                            </div>
+                            <div
+                                className={styles.button}
+                                onClick={handleLogoutClick}
+                            >
+                                Logout
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
--- front/src/features/auth/login/loginForm/index.tsx
+++ front/src/features/auth/login/loginForm/index.tsx
@@ -6,42 +6,42 @@ import { loginRequest } from '../../authSlice';
 import styles from './loginForm.module.scss';
 
 function LoginForm() {
-  const {
-    handleSubmit,
-    register,
-    formState: { dirtyFields },
-  } = useForm<LoginRequest>({
-    defaultValues: { email: 'david.mosca69@gmail.com', password: 'pwd' },
-  });
-  const dispatch = useAppDispatch();
-  const onSubmit: SubmitHandler<LoginRequest> = (data) => {
-    // if (dirtyFields.email && dirtyFields.password) {
-    dispatch(loginRequest(data));
-    // }
-  };
+    const {
+        handleSubmit,
+        register,
+        formState: { dirtyFields },
+    } = useForm<LoginRequest>({
+        defaultValues: { email: 'david.admin@gmail.com', password: 'pwd' },
+    });
+    const dispatch = useAppDispatch();
+    const onSubmit: SubmitHandler<LoginRequest> = (data) => {
+        // if (dirtyFields.email && dirtyFields.password) {
+        dispatch(loginRequest(data));
+        // }
+    };
 
-  return (
-    <div className={styles.loginForm}>
-      <form method="post" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="email">Login</label>
-          <InputCustom type="text" {...register('email')} />
+    return (
+        <div className={styles.loginForm}>
+            <form method="post" onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="email">Login</label>
+                    <InputCustom type="text" {...register('email')} />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Mot de passe</label>
+                    <InputCustom type="password" {...register('password')} />
+                </div>
+
+                <button
+                    type="submit"
+                    // disabled={!dirtyFields.email || !dirtyFields.password}
+                >
+                    Se connecter
+                </button>
+            </form>
         </div>
-
-        <div>
-          <label htmlFor="password">Mot de passe</label>
-          <InputCustom type="password" {...register('password')} />
-        </div>
-
-        <button
-          type="submit"
-          // disabled={!dirtyFields.email || !dirtyFields.password}
-        >
-          Se connecter
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default LoginForm;
